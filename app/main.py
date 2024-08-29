dc362a7466e1dfd8a515ab9ab72ba1623b673a96
Tham Tra <tham.tt@morsoftware.com>
2024-08-30 00:19:27 +0700

    update geoip2 reader

--- app/main.py
+++ app/main.py
@@ -1,25 +1,36 @@
-from typing import Union
 from fastapi import FastAPI
-import requests
-import ipaddress
+import geoip2.database
+from geoip2.errors import AddressNotFoundError
+import pathlib
 
 app = FastAPI()
 
 
+def read_ip(your_ip: str, reader: str):
+    try:
+        path = pathlib.Path(__file__).parent.resolve()
+        with geoip2.database.Reader(f"{path}/data/{reader}/GeoLite2-{reader.title()}.mmdb") as r:
+            if reader == 'city':
+                response = r.city(your_ip)
+            else:
+                response = r.country(your_ip)
+        return response.raw
+    except ValueError as e:
+        return {
+            "error": str(e)
+        }
+    except AddressNotFoundError as e:
+        return {
+            "AddressNotFoundError": str(e)
+        }
+
+
 @app.get("/")
-def read_root():
+def index():
     return {"message": "Pong!"}
 
 
 @app.get("/ip/{your_ip}")
 def read_item(your_ip: str):
-    try:
-        ipaddress.IPv4Network(your_ip)
-        requests.put(f"http://es01:9200/user_ip/_doc/{your_ip}?pipeline=geoip&pretty", json = {"ip": your_ip})
-        r = requests.get(f"http://es01:9200/user_ip/_doc/{your_ip}?pretty")
-        return r.json()['_source']
-
-    except ValueError as e:
-        return {
-            "error": str(e)
-        }
+    reader = 'city'
+    return read_ip(your_ip, reader)
